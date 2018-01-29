3e89a1c5f50368aa6e220612532680df253c7a6d
andino <andy.andino93@gmail.com>
2018-01-29 10:56:33 -0800

    webpack dev config

---
+++ webpack.dev.config.js
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const DIST_DIR = path.resolve(__dirname, 'client/dist');
+const SRC_DIR = path.resolve(__dirname, 'client/src');
+
+const config = {
+  resolve: { extensions: ['.webpack.js', '.web.js', '.js', '.json', '.jsx'] },
+  entry: ['webpack-hot-middleware/client?reload=true', `${SRC_DIR}/components/Main.jsx`],
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+    publicPath: '/client/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env'],
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
