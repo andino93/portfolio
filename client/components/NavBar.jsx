b735e838d4d2de675af82acb9f2d63605f8d998c
andino <andy.andino93@gmail.com>
2018-02-04 13:17:47 -0800

    add navbar class again

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div className="navbar is-light">
+    <div className="navbar-item">Home</div>
+    <div className="navbar-item">Expertise</div>
+    <div className="navbar-item">Projects</div>
+    <div className="navbar-item">About</div>
+    <div className="navbar-item">Contact</div>
+  </div>
+);
+
+export default NavBar;
