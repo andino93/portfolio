6e7273afd26aa22b3a87a371377a0c58584a4033
andino <andy.andino93@gmail.com>
2018-02-04 12:19:04 -0800

    add selector for project tables

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -4,7 +4,17 @@ const Projects = () => (
   <div className="hero is-large is-bold is-light">
     <div className="hero-body">
       <div className="container">
-        Projects
+        <div className="title level-item">Projects</div>
+        <div className="container">
+          <div className="tabs is-centered is-large is-boxed is-fullwidth">
+            <ul>
+              <li className="is-active"><a>All</a></li>
+              <li><a>Node</a></li>
+              <li><a>Javascript</a></li>
+            </ul>
+          </div>
+          Add some React stuff here for projects. Maybe I'll need some state
+        </div>
       </div>
     </div>
   </div>
