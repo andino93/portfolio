a19ec20f64eada30037afe4ea163b91b3ad4da5f
andino <andy.andino93@gmail.com>
2018-02-03 17:29:41 -0800

    remove nav buttons in preparation of removing navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -14,35 +14,6 @@ const NavBar = () => (
           width="112"
           height="28"
         />
-        <div className="navbar-menu">
-          <a className="navbar-item" href="https://github.com/andino93">
-            <img
-              src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
-              alt="GitHub Logo"
-              width="30"
-              height="50"
-            />
-            Github
-          </a>
-          <a className="navbar-item" href="https://www.linkedin.com/in/andino93/">
-            <img
-              src="http://www.pngall.com/wp-content/uploads/2016/07/Linkedin.png"
-              alt="Linkedin Logo"
-              width="30"
-              height="50"
-            />
-            Linkedin
-          </a>
-          <a className="navbar-item" href="mailto:andrew.at.nguyen@gmail.com">
-            <img
-              src="https://cdn.pixabay.com/photo/2013/07/12/13/55/letter-147563_1280.png"
-              alt="email"
-              width="30"
-              height="50"
-            />
-            Email
-          </a>
-        </div>
       </div>
       <button className="button navbar-burger">
         <span />
