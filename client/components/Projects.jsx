7e7d0497a8f18f6a4c09e346798e73aa383744fb
andino <andy.andino93@gmail.com>
2018-02-04 13:04:32 -0800

    change hero size

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 const Projects = () => (
-  <div className="hero is-large is-bold is-light">
+  <div className="hero is-bold is-light">
     <div className="hero-body">
       <div className="container">
         <div className="title level-item">Projects</div>
         <div className="container">
-          <div className="tabs is-centered is-large is-boxed is-fullwidth">
+          <div className="tabs is-centered is-large is-boxed is-fullwidth is-mobile">
             <ul>
               <li className="is-active"><a>All</a></li>
-              <li><a>Node</a></li>
-              <li><a>Javascript</a></li>
+              <li><a>X-PEAs</a></li>
+              <li><a>rocket!</a></li>
+              <li><a>WhaleThen</a></li>
+              <li><a>Fam.ly</a></li>
+              <li><a>DooToo</a></li>
             </ul>
           </div>
           Add some React stuff here for projects. Maybe I'll need some state
+          <div className="hero is-large">
+            <div className="hero-body">
+              Test check to make sure unsized hero can change size dynamically
+            </div>
+          </div>
         </div>
       </div>
     </div>
