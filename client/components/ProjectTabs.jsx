27fc52b39087cba91c708e7cd778edda27a778b5
andino <andy.andino93@gmail.com>
2018-02-06 10:28:15 -0800

    change tabs to sort by framework

--- client/components/ProjectTabs.jsx
+++ client/components/ProjectTabs.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/anchor-is-valid */
+// Dividers by project
+// const ProjectTabs = props => (
+//   <div className="tabs is-centered is-large is-boxed is-fullwidth is-mobile">
+//     <ul onClick={e => props.onSelect(e.target.className)} onKeyUp={props.onKeyPress}>
+//       <li className={props.isSelected('all')}><a className="all">All</a></li>
+//       <li className={props.isSelected('x-peas')}><a className="x-peas">X-PEAs</a></li>
+//       <li className={props.isSelected('rocket')}><a className="rocket">rocket!</a></li>
+//       <li className={props.isSelected('whalethen')}><a className="whalethen">WhaleThen</a></li>
+//       <li className={props.isSelected('famly')}><a className="famly">Fam.ly</a></li>
+//       <li className={props.isSelected('dootoo')}><a className="dootoo">DooToo</a></li>
+//     </ul>
+//   </div>
+// );
+
+// dividers by framework
 const ProjectTabs = props => (
   <div className="tabs is-centered is-large is-boxed is-fullwidth is-mobile">
     <ul onClick={e => props.onSelect(e.target.className)} onKeyUp={props.onKeyPress}>
       <li className={props.isSelected('all')}><a className="all">All</a></li>
-      <li className={props.isSelected('x-peas')}><a className="x-peas">X-PEAs</a></li>
-      <li className={props.isSelected('rocket')}><a className="rocket">rocket!</a></li>
-      <li className={props.isSelected('whalethen')}><a className="whalethen">WhaleThen</a></li>
-      <li className={props.isSelected('famly')}><a className="famly">Fam.ly</a></li>
-      <li className={props.isSelected('dootoo')}><a className="dootoo">DooToo</a></li>
+      <li className={props.isSelected('nodejs')}><a className="nodejs">Node.js</a></li>
+      <li className={props.isSelected('react')}><a className="react">React</a></li>
+      <li className={props.isSelected('angular')}><a className="angular">Angular</a></li>
     </ul>
   </div>
 );
