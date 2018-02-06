cd35c18b3a50486b35c560f01681ecaea14fbf17
andino <andy.andino93@gmail.com>
2018-02-06 10:29:11 -0800

    add folder for assets

---
+++ client/assets/projects.js
@@ -0,0 +1,18 @@
+/**
+  TODO:
+    * data for each type of Project
+    * description
+    * title
+    * tech stack
+    * launched URL if applicable
+    * github URL
+*/
+const projects = {
+  'x-peas': {},
+  rocket: {},
+  whalethen: {},
+  famly: {},
+  dootoo: {},
+};
+
+export default projects;
